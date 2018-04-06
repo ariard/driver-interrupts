d04c2edd84f0e14a02f890216ae3515f30f2e80b
Antoine Riard <ariard@student.42.fr>
2018-04-06 19:21:09 +0200

    [usb_driver] registering device ok

--- module_keylogger.c
+++ module_keylogger.c
@@ -13,6 +13,8 @@ MODULE_DESCRIPTION("keylogger module");
 # define KEYBOARD_VENDOR_ID	0x05ac
 # define KEYBOARD_PRODUCT_ID	0x024f
 
+# define KEYBOARD_MINOR_BASE	0
+
 static ssize_t keylogger_read(struct file *filp, char __user *buffer,
 				size_t length, loff_t *offset)
 {
@@ -40,15 +42,39 @@ static struct miscdevice keylogger_misc = {
 	.fops = &keylogger_misc_fops,
 };
 
+static char *keyboard_devnode(struct device *dev, umode_t *mode)
+{
+	return kasprintf(GFP_KERNEL, "usb/%s", dev_name(dev));
+}
+
+static struct usb_class_driver keyboard_class = {
+	.name = "keyboard-log",
+	.devnode = keyboard_devnode,
+	.fops = &keylogger_misc_fops,
+	.minor_base = KEYBOARD_MINOR_BASE,
+};
+
 static int keyboard_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
+	static int	state;
+	int		result;
+
 	printk(KERN_INFO "keyboard driver : probing for %04X:%04X\n", id->idVendor, id->idProduct);
+	if (!state) {
+		result = usb_register_dev(interface, &keyboard_class);
+		printk(KERN_INFO "keyboard driver : register %d\n", result);
+		state = (result >= 0) ? 1 : 0;
+	}
+	// check if device already in driver list usb_driver->dynids
+/*	usb_register_dev(struct usb_interface *intf, struct usb_class_driver *class_driver) */
+	// kebyoard_driver		
 	return 0;
 }
 
 static void keyboard_disconnect(struct usb_interface *interface)
 {
 	printk(KERN_INFO "keyboard driver : keyboard remove\n");
+	usb_deregister_dev(interface, &keyboard_class);
 }
 
 static struct usb_device_id keyboard_table[] = {
