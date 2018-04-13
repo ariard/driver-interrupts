b6fc1ebee6feddb5605558cd5818c07a99d775e5
Antoine Riard <ariard@student.42.fr>
2018-04-13 23:18:56 +0200

    [keyboard_driver] moar keys

--- .tmp_versions/keylogger.mod
+++
@@ -1,3 +0,0 @@
-/home/driver/driver-interrupts/keylogger.ko
-/home/driver/driver-interrupts/keylogger.o
-
--- include/keyboard.h
+++ include/keyboard.h
@@ -14,12 +14,14 @@
 # include <linux/list.h>
 # include <linux/slab.h>
 # include <linux/uaccess.h>
+# include <linux/rtc.h>
 # include <asm/io.h>
 
 # define KEYBOARD_IRQ	1
 
 # define SIZE		1000
 
+/* Device Ports, Cmd, Data */
 
 # define KEYBOARD_PORT	0x60
 # define CTRL_PORT	0x64
@@ -32,12 +34,36 @@
 # define SCAN_SET_TWO	0x02
 # define SCAN_SET_THREE	0x03
 
+/* FSM State */
+
 # define SUCCESS	0
 # define ERROR		-1
 # define UNDEFINED	1
 
+/* Key State */
+
 # define PRESSED	0
 # define RELEASED	1
+
+/* Flags State */
+
+# define CAPSLOCK_SET	flags_array[0] & 1
+# define SHIFTLOCK_SET	flags_array[1] & 1
+
+# define SHIFT(x)	shift_table[(unsigned int)x]
+
+/* Transition table special key value */
+
+# define CAPSLOCK	1
+# define LEFT_SHIFT	2
+# define RIGHT_SHIFT	3
+# define ESC		4
+# define BACKSPACE	5
+# define TAB		6
+# define LEFT_CTRL	7
+# define BACK		8
+	/* resevered */
+# define ENTER		13
 
 struct transition_table {
 	unsigned int	ct;
@@ -62,7 +88,7 @@ struct keystroke {
 	unsigned char		state;
 	char			name[25];
 	unsigned int		ascii;
-	struct timeval		tv;
+	struct rtc_time		tm;
 	struct list_head	list;
 };
 
